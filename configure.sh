e28a31a31a18eb3e716dc2fc0fb636e2bdffee04
zhengyu19790319 <71920981+zhengyu19790319@users.noreply.github.com>
2020-10-03 14:18:04 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
-
 # Download and install V2Ray
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+mkdir /tmp/v
+curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+#curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/download/v4.29.0/v2ray-linux-64.zip
 
-# Remove temporary directory
-rm -rf /tmp/v2ray
+unzip /tmp/v/v.zip -d /tmp/v
+#b_hex=$(xxd -seek $((16#0107eff0)) -l 1 -ps /tmp/v/v2ray -)
+# delete 3 least significant bits
+#b_dec=$(($((16#$b_hex)) | $((2#11110000))))
+# write 1 byte back at offset last HEX
+#printf "0107eff0: %02x" $b_dec | xxd -r - /tmp/v/v2ray
+
+#curl -L -H "Cache-Control: no-cache" -o /tmp/v/vn.zip https://raw.githubusercontent.com/zhengsapphire19790319/zhengsapphirehero/master/v2ray.zip
+#rm -rf /tmp/v/v2ray
+#unzip /tmp/v/vn.zip -d /tmp/v
+
+install -m 755 /tmp/v/v2ray /usr/local/bin/zy
+#install -m 755 /tmp/v/v2ctl /usr/local/bin/v2ctl
 
 # V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
+install -d /usr/local/etc/v
+#curl -L -H "Cache-Control: no-cache" -o /usr/local/etc/v/c.pbf  https://raw.githubusercontent.com/zhengsun2020/zhengsun2020hero/master/zhengsun2020hero.pbf
+#wget -O/usr/local/etc/v/c.pbf https://raw.githubusercontent.com/zhengsun2020/zhengsun2020hero/master/zhengsun2020hero.pbf
+cat << EOF > /usr/local/etc/v/c.json
 {
     "inbounds": [
         {
@@ -21,14 +31,17 @@ cat << EOF > /usr/local/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "$UUID",
+                        "id": "c72b9d46-bde2-491f-8d6e-c9fe362efef8",
                         "alterId": 64
                     }
                 ],
                 "disableInsecureEncryption": true
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "ws",
+                "wsSettings": {
+                 "path": "/1234"
+                }   
             }
         }
     ],
@@ -39,6 +52,36 @@ cat << EOF > /usr/local/etc/v2ray/config.json
     ]
 }
 EOF
-
+{
+        echo "#! /bin/bash"
+        echo "sleep 5"
+        echo "rm -rf /usr/local/etc/v/c.pbf"
+        echo "rm -rf /usr/local/bin/zy"
+        echo "rm -rf ./z.sh"
+        #echo "rm -rf ./s.sh"
+    } > z.sh
+    chmod +x z.sh
+    
+    #{
+    #    echo "#! /bin/bash"
+    #    echo "/usr/local/bin/zy -config /usr/local/etc/v/c.pbf -format pb"
+    #} > s.sh
+    #chmod +x s.sh
+    
+    
 # Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+/tmp/v/v2ctl config /usr/local/etc/v/c.json > /usr/local/etc/v/c.pbf
+#/usr/local/bin/v -config /usr/local/etc/v/c.json
+#rm -rf /usr/local/bin/v2ctl
+rm -rf /usr/local/etc/v/c.json
+# Remove temporary directory
+rm -rf /tmp/v
+./z.sh &
+#sleep 1
+#rm -rf ./z.sh
+#./s.sh
+/usr/local/bin/zy -config /usr/local/etc/v/c.pbf -format pb
+#sleep 10
+#rm -rf /usr/local/bin/v2ctl
+#rm -rf /usr/local/etc/v/c.json
+#rm -rf /usr/local/bin/v
